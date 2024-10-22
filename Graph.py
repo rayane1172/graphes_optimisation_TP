e07687be6ed22c435e759927ea582efc21c270ed
rayane-bouchair <bouchairrayane01@gmail.com>
2024-10-22 22:36:08 +0100

    adding graph class & BFS & DFS

---
+++ Graph.py
@@ -0,0 +1,136 @@
+from collections import deque
+
+
+class Graph:
+   def __init__(self, graph_dict=None):
+      if graph_dict is None:
+         graph_dict = {}
+      self.graph = graph_dict
+
+# add edge btw two nodes
+   def add_edge(self, node1, node2):
+      if node1 not in self.graph:
+         self.graph[node1] = []
+      if node2 not in self.graph:
+         self.graph[node2] = []
+
+      if node2 not in self.graph[node1]: # not repeat the node in list of (voisins)
+         # print(f"\n --> {type(self.graph[node1])}<=")
+         self.graph[node1].append(node2)
+      if node1 not in self.graph[node2]:
+         self.graph[node2].append(node1)
+
+   def display_graph(self):
+      for node, neighbors in self.graph.items():
+         print(f"{node} : {neighbors}")
+
+   def BFS(self, start_node):
+      laFile = deque()
+      ordered_list = []
+      laFile.append(start_node)
+
+
+      visited = {node: -1 for node in self.graph} # all values with -1
+      visited[start_node] = 0
+
+      # visited = {
+      #    start_node: 0
+      #    }
+
+      while len(laFile) > 0:
+         racine = laFile.popleft()
+         current_distance = visited[racine]
+         ordered_list.append(racine) # to stock the result of FIFO
+
+         for voisin in self.graph[racine]:
+            if visited[voisin] == -1:
+               laFile.append(voisin) # enfiler les voisins des racine precedant
+               visited[voisin] = current_distance + 1
+
+      # inaccessible = False
+      # for node in visited.keys(): # when a node is not connected with the starting node
+      #    # if node not in visited:
+      #    if visited[node] == -1:
+      #       # visited[node] = 0
+      #       inaccessible = True
+
+      # print(f"-----> {ordered_list}")
+      # if inaccessible:
+      #    # print(f"Node -> {start_node} is inaccessible")
+      #    return f"Node -> {start_node} is inaccessible"
+      print(f"ordered list -> {ordered_list}")
+      return visited
+
+
+# todo: ////////////////////////////////////////////////////////
+
+   def is_tree(self):
+      # three conditions for a tree :
+      # nbr edges = n -1
+      # without cycls
+      # connex -> visit all graph nodes
+
+      if not self.graph:
+         return False
+      nbr = len(self.graph)
+      edge_nbr = sum(len(voisin) for voisin in self.graph.values()) // 2
+
+      if edge_nbr != nbr - 1: # nbr arcs < node - 1
+         return False
+
+      visited = set() # track visited node
+      def DFS(node, parent): # cherche for cycles
+         visited.add(node)
+         for voisin in self.graph[node]:
+            if voisin not in visited:
+               if not DFS(voisin, node):
+                  return False
+            elif voisin != parent: #todo -->  bcz of graph indirectionnel -> verification des cycles
+               return False
+         return True
+
+      first_node = list(self.graph.keys())[0] # first case of list of nodes (keys) : so first node in graph
+      if not DFS(first_node,None):
+         return False
+
+      if len(visited) != nbr: #todo--> visit all nodes
+         return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+      # def dfs2(self, start_node): # not completted 
+      #    stack = [] # empty stack
+      #    stack.append(start_node)
+      #    ordered_list = []
+      #    visited = {
+      #       start_node : 0
+      #    }
+
+      #    while len(stack) > 0:
+      #       racine = stack.pop()
+      #       ordered_list.append(racine)
+      #       current_distance = visited[racine]
+
+      #       for voisin in sorted(self.graph[racine], reverse=True):
+      #          if voisin not in visited:
+      #             stack.append(voisin)
+      #             visited[voisin] = current_distance + 1
+      #       inacessible = False
+      #       for node in self.graph.keys():
+      #          if node not in visited:
+      #             inacessible = True
+
+      #       if inacessible:
+      #          print(f"node { start_node} incaccessible ")
+      #    return visited
+      return True
---
+++ main.py
@@ -0,0 +1,81 @@
+from collections import deque
+from Graph import Graph
+
+exit_program = False
+mon_graph = Graph()
+
+
+test_graph = {
+    "A": ["B", "E"],
+    "B": ["C", "A","D"],
+    "C": ["B","E"],
+    "D": ["B"],
+    "E": ["A","C"]
+}
+
+test_graph_is_tree = {
+    "A": ["B", "C"],
+    "B": ["A", "D", "E"],
+    "C": ["A"],
+    "D": ["B"],
+    "E": ["B"]
+}
+
+test_graph = Graph(test_graph)
+test_graph_is_tree = Graph(test_graph_is_tree)
+
+test_graph.display_graph()
+print(test_graph.BFS(start_node="A"))
+print(test_graph.is_tree())
+print("-"*15)
+test_graph_is_tree.display_graph()
+print(test_graph_is_tree.BFS(start_node="A"))
+print(test_graph_is_tree.is_tree())
+# while not exit_program:
+#     print("Donner moi votre graphe d'abord ".center(20, "*"))
+#     n = int(input("Donner moi le nombre de nœuds -> "))
+
+#     # Generate nodes using letters (A, B, C, ...)
+#     nodesName = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+#     for i in range(n):
+#         print(f"Node-{nodesName[i]}")
+#         node1 = nodesName[i]
+#         node2_input = input("|-> Go to (separated nodes names with -,-) -> ")
+#         node2_list = node2_input.split(",")
+
+#         for node2 in node2_list:
+#             node2 = node2.strip()
+#             if node2 == "": continue # input vide of nodes relation
+#             if node2 in nodesName :  # Ensure the input node exists in the generated nodes (alphabet majiscule)
+#                 mon_graph.add_edge(node1, node2)
+#             else:
+#                 print(f"Node {node2} does not exist. Please use valid node names.")
+
+#     mon_graph.display_graph()
+
+#     while True:
+#         print("Menu:".center(20,"*"))
+#         print("1. BFS")
+#         print("2. Check if the graph is a tree")
+#         print("3. Exit")
+#         choice = input("Choose an option (1, 2, or 3) -> ")
+
+#         if choice == '1':
+#                 start_node = input("\nEnter the start node for BFS -> (A,B,C...)->")
+#                 if start_node in mon_graph.graph:
+#                     bfs_result = mon_graph.BFS(start_node)
+#                     print(f"\n\t--> BFS result: {bfs_result}")
+#                 else:
+#                     print("Node not found in the graph.")
+#         elif choice == '2':
+#                 if mon_graph.is_tree():
+#                     print("----> The graph is a tree.")
+#                 else:
+#                     print("----> The graph is not a tree.")
+#         elif choice == '3':
+#             exit_program = True
+#             print("Exiting the program.")
+#             break
+#         else:
+#             print("Invalid choice, please try again.")
